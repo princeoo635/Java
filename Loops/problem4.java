06b9a169ef486e12deea43821a4c25766d70048c
princeoo635 <princeoo635@gmail.com>
2026-05-02 18:57:02 +0530

    printing name 100 times

---
+++ Loops/problem4.java
@@ -0,0 +1,12 @@
+// print your name 100 times
+
+import java.util.Scanner;
+
+public class problem4 {
+    public static void main(String[] args) {
+    
+    for (int i=1;i<=100;i++){
+        System.out.println("Prince");
+    }
+   } 
+}
