f14d5bb8aef113650d30ad00c67af7f228e85e74
princeoo635 <princeoo635@gmail.com>
2026-05-06 22:17:19 +0530

    Add problem8.java to print all integers in the range from 50 to 100 that are perfectly divisible by 7

---
+++ Loops/problem8.java
@@ -0,0 +1,10 @@
+// print all integer in range from 50 to 100 that are perfectly divisible by 7
+public class problem8 {
+   public static void main(String[] args) {
+    for(int i=50;i<=100;i++){
+        if(i%7==0){
+            System.out.println(i);
+        }
+    }
+   } 
+}
