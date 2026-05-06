059cc2a809301b829ca1ae6089020fe080d4ba44
princeoo635 <princeoo635@gmail.com>
2026-05-06 22:04:22 +0530

    Add problem6.java to print all even numbers from 1 to 100

---
+++ Loops/problem6.java
@@ -0,0 +1,8 @@
+// print all the even numbers from 1 to 100
+public class problem6 {
+    public static void main(String[] args) {
+        for(int i=2;i<=100;i+=2){
+            System.out.print(i+" ");
+        }
+    }
+}
